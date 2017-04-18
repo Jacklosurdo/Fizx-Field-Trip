b7fea97725ef6e8e4622be6d07883b4b120a0be3
Student <student@jlosurdo.local>
2017-04-18 10:39:57 -0500

    just created map view and works, put map to front of six flags

--- Fizx Field Trip/ViewController.swift
+++ Fizx Field Trip/ViewController.swift
@@ -11,16 +11,42 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, MKMapViewDelegate
 {
-
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    let sixFlagsCoordiantes = CLLocationCoordinate2DMake(42.369680, -87.935544)
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        myMapView.delegate = self
+        addPinToMap()
     }
-
-
-
+    
+    func addPinToMap()
+    {
+        let myAnnotation = MKPointAnnotation()
+        myAnnotation.coordinate = sixFlagsCoordiantes
+        myAnnotation.title = "6 flags"
+        myAnnotation.subtitle = "Carousel Plaza"
+        myMapView.addAnnotation(myAnnotation)
+    }
+    
+    @IBAction func goToSixFlags(_ sender: Any)
+    {
+        centerMap(location: sixFlagsCoordiantes)
+    }
+    func centerMap(location: CLLocationCoordinate2D)
+    {
+        let mySpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let myRegion = MKCoordinateRegion(center: location, span: mySpan)
+        myMapView.setRegion(myRegion, animated: true)
+    }
+    
+    
+    
 }
+
 
